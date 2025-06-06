738b314cb266c18865706ae6cea9e23a409840e1
happ-2h <hikonetgames@gmail.com>
2025-06-05 20:02:13 -0700

    Client title screen cursor selection

--- client/src/game/state/TitleScreenState.js
+++ client/src/game/state/TitleScreenState.js
@@ -16,8 +16,11 @@ export default class TitleScreenState extends State {
     super();
 
     this.#btn_play = new Button(120, 24, 8, 3, "Play", 16, 5);
+    this.#btn_play.callback = () => { console.log("PLAY"); }
     this.#btn_create = new Button(120, 40 + 8*3, 8, 3, "create", 7, 5);
+    this.#btn_create.callback = () => { console.log("CREATE"); }
     this.#btn_settings = new Button(120, 40 + 8 * 8, 8, 3, "settings", 0, 5);
+    this.#btn_settings.callback = () => { console.log("SETTINGS"); }
 
     this.#cursor = new Cursor(120 - TILE_SIZE, 24 + 16, 0.3, 0, 208);
   }
@@ -42,7 +45,25 @@ export default class TitleScreenState extends State {
         this.#cursor.y = clamp(this.#cursor.y + 40, 24, (40 + 8 * 8) + 16);
       }
     }
+    else if (KeyHandler.isDown(13)) {
+      if (this.#cursor.timer >= this.#cursor.delay) {
+        if (this.#cursor.y <= this.#btn_play.y + this.#btn_play.height*8) {
+          this.#btn_play.callback();
+          this.#cursor.timer = 0;
+        }
+        else if (this.#cursor.y <= this.#btn_create.y + this.#btn_create.height*8) {
+          this.#btn_create.callback();
+          this.#cursor.timer = 0;
+        }
+        else if (this.#cursor.y <= this.#btn_settings.y + this.#btn_settings.height*8) {
+          this.#btn_settings.callback();
+          this.#cursor.timer = 0;
+        }
+      }
 
+    }
+
+    // Animation
     this.#cursor.x = (120 - TILE_SIZE) + (4*Math.cos(this.#cursor.timer * 20));
   }
 
