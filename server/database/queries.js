17349907697db487b5bc7e29dfbaf92cbd928996
happ-2h <hikonetgames@gmail.com>
2025-06-08 14:41:46 -0700

    Server removed user registration requirement

--- server/database/queries.js
+++ server/database/queries.js
@@ -2,34 +2,6 @@ import { DatabaseSync } from "node:sqlite";
 
 const database = new DatabaseSync(`${import.meta.dirname}/nononline.db`);
 
-/*const initDB = `
-CREATE TABLE IF NOT EXISTS users (
-  user_id TEXT PRIMARY KEY NOT NULL UNIQUE,
-  username TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
-)
-`;
-
-database.exec(initDB);
-
-const createUser = database.prepare(`
-INSERT INTO users (user_id, username, password)
-VALUES (?, ?, ?)
-RETURNING user_id, username
-`);
-
-const getUsername = database.prepare(`
-SELECT user_id, username FROM users WHERE username = ?
-`);
-
-const getUserID = database.prepare(`
-SELECT user_id, username FROM users WHERE user_id = ?
-`);
-
-const getUsernamePassword = database.prepare(`
-SELECT user_id, username, password FROM users WHERE username = ?
-`);*/
-
 /*
   MAYBE rating db and date created
  */
@@ -70,10 +42,6 @@ LIMIT 1
 `);
 
 export {
-  /*createUser,
-  getUsername,
-  getUserID,
-  getUsernamePassword*/
   createPuzzle,
   getPuzzle,
   getPuzzles,
--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 import express from "express";
-// import usersRouter from "./routes/users.routes.js";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import helmet from "helmet"
 import 'dotenv/config';
 import puzzlesRouter from "./routes/puzzles.routes.js";
@@ -11,9 +9,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
 
-// app.use('/api/users', usersRouter);
 app.use("/api/puzzles", puzzlesRouter);
 
 app.listen(
