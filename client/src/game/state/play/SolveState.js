92ef93a6a6390c13341f6bba7b0d77c5732dd9fc
happ-2h <hikonetgames@gmail.com>
2025-06-12 10:52:39 -0700

    Client Solve state refactored

--- client/src/game/state/play/SolveState.js
+++ client/src/game/state/play/SolveState.js
@@ -1,7 +1,6 @@
 import Renderer from "../../../gfx/Renderer";
 import Cursor from "../../../gfx/ui/Cursor";
 import KeyHandler from "../../../input/KeyHandler";
-import { clamp } from "../../../math/utils";
 import { TILE_SIZE } from "../../constants";
 import State from "../State";
 import StateHandler from "../StateHandler";
@@ -91,7 +90,12 @@ export default class SolveState extends State {
     this.#nums_cols.forEach(arr => arr.reverse());
     this.#nums_rows.forEach(arr => arr.reverse());
 
-    this.#cursor = new Cursor(50, 50, 0.2, 8, 192);
+    this.#cursor = new Cursor(
+      50 - 8, 50,
+      50 - 8, 50 + 8 * (this.#board[0].length-2),
+      50, 50 + 8 * (this.#board.length - 1),
+      8, 0.3, false
+    );
   }
 
   onEnter() {}
@@ -100,40 +104,16 @@ export default class SolveState extends State {
   init() {}
 
   update(dt) {
-    this.#cursor.timer += dt;
+    this.#cursor.update(dt);
 
-    if (KeyHandler.isDown(37)) {
-      if (this.#cursor.timer >= this.#cursor.delay) {
-        this.#cursor.timer = 0;
-        this.#cursor.x = clamp(this.#cursor.x - 8, 50, 50 + 8 * (this.#board[0].length-1));
-      }
-    }
-    else if (KeyHandler.isDown(39)) {
-      if (this.#cursor.timer >= this.#cursor.delay) {
-        this.#cursor.timer = 0;
-        this.#cursor.x = clamp(this.#cursor.x + 8, 50, 50 + 8 * (this.#board[0].length-1));
-      }
-    }
-    else if (KeyHandler.isDown(38)) {
-      if (this.#cursor.timer >= this.#cursor.delay) {
-        this.#cursor.timer = 0;
-        this.#cursor.y = clamp(this.#cursor.y - 8, 50, 50 + 8 * (this.#board.length-1));
-      }
-    }
-    else if (KeyHandler.isDown(40)) {
-      if (this.#cursor.timer >= this.#cursor.delay) {
-        this.#cursor.timer = 0;
-        this.#cursor.y = clamp(this.#cursor.y + 8, 50, 50 + 8 * (this.#board.length-1));
-      }
-    }
+    const cursorx = ((this.#cursor.x - 50) / 8) + 1;
+    const cursory = (this.#cursor.y - 50) / 8;
 
     // Markers
     // - Set
     if (KeyHandler.isDown(49)) {
       if (this.#cursor.timer >= this.#cursor.delay) {
         this.#cursor.timer = 0;
-        let cursorx = (this.#cursor.x - 50) / 8;
-        let cursory = (this.#cursor.y - 50) / 8;
 
         this.#board[cursory][cursorx] = this.#board[cursory][cursorx] !== 1 ? 1 : 0;
 
@@ -147,8 +127,6 @@ export default class SolveState extends State {
     else if (KeyHandler.isDown(50)) {
       if (this.#cursor.timer >= this.#cursor.delay) {
         this.#cursor.timer = 0;
-        let cursorx = (this.#cursor.x - 50) / 8;
-        let cursory = (this.#cursor.y - 50) / 8;
 
         this.#board[cursory][cursorx] = this.#board[cursory][cursorx] !== 2 ? 2 : 0;
       }
@@ -157,8 +135,6 @@ export default class SolveState extends State {
     else if (KeyHandler.isDown(51)) {
       if (this.#cursor.timer >= this.#cursor.delay) {
         this.#cursor.timer = 0;
-        let cursorx = (this.#cursor.x - 50) / 8;
-        let cursory = (this.#cursor.y - 50) / 8;
 
         this.#board[cursory][cursorx] = this.#board[cursory][cursorx] !== 3 ? 3 : 0;
       }
@@ -198,13 +174,9 @@ export default class SolveState extends State {
     // - Cols
     for (let y = 0; y < this.#nums_cols.length; ++y) {
       // 0
-      if (this.#nums_cols[y].length === 0) {
-        Renderer.image(
-          "spritesheet",
-          0, 248, TILE_SIZE, TILE_SIZE,
-          50 + y * 8, 50-8, TILE_SIZE, TILE_SIZE
-        );
-      }
+      if (this.#nums_cols[y].length === 0)
+        Renderer.imageText("0", 50 + y * 8, 50 - 8);
+
       for (let x = 0; x < this.#nums_cols[y].length; ++x) {
         let n = this.#nums_cols[y][x];
 
@@ -226,41 +198,39 @@ export default class SolveState extends State {
             4, 8
           );
         }
-        else {
-          Renderer.image(
-            "spritesheet",
-            n * 8, 248, TILE_SIZE, TILE_SIZE,
-            50 + (y * 8),
-            50 - 8 - x * 8,
-            TILE_SIZE, TILE_SIZE
-          );
-        }
+        else
+          Renderer.imageText(n, 50 + y * 8, 50 - 8 - x * 8);
       }
     }
     // - Rows
     for (let y = 0; y < this.#nums_rows.length; ++y) {
       // 0
-      if (this.#nums_rows[y].length === 0) {
-        Renderer.image(
-          "spritesheet",
-          0, 248, TILE_SIZE, TILE_SIZE,
-          50 -8, 50 + y * 8, TILE_SIZE, TILE_SIZE
-        );
-      }
+      if (this.#nums_rows[y].length === 0)
+        Renderer.imageText("0", 50-8, 50 + y * 8);
+
       for (let x = 0; x < this.#nums_rows[y].length; ++x) {
         let n = this.#nums_rows[y][x];
 
         if (n >= 10) {
-        }
-        else {
+          // 1
           Renderer.image(
             "spritesheet",
-            n * 8, 248, TILE_SIZE, TILE_SIZE,
-            50-8 - (x * 8),
-            50 + (y * 8),
-            TILE_SIZE, TILE_SIZE
+            84, 248, 4, 4,
+            50 - 8 - (x * 8),
+            50 + y * 8,
+            4, 8
+          );
+          // One's place
+          Renderer.image(
+            "spritesheet",
+            80 + (n%10) * 4, 248, 4, 4,
+            50 - 4 - (x * 8),
+            50 + y * 8,
+            4, 8
           );
         }
+        else
+          Renderer.imageText(n, 50 - 8 - (x * 8), 50 + y * 8);
       }
     }
 
