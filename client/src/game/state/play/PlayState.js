e24fd740de6f954835d0a55f60dd873b84c043d5
happ-2h <hikonetgames@gmail.com>
2025-06-12 10:10:10 -0700

    Client Play state refactored

--- client/src/game/state/play/PlayState.js
+++ client/src/game/state/play/PlayState.js
@@ -1,7 +1,5 @@
 import Cursor from "../../../gfx/ui/Cursor";
 import Label from "../../../gfx/ui/Label";
-import KeyHandler from "../../../input/KeyHandler";
-import { clamp } from "../../../math/utils";
 import State from "../State";
 import StateHandler from "../StateHandler";
 import SelectPuzzleState from "./SelectPuzzleState";
@@ -17,7 +15,7 @@ export default class PlayState extends State {
   constructor() {
     super();
 
-    this.#label_top10 = new Label(90, 0, "Top 10");
+    this.#label_top10 = new Label("top 10", 90, 0);
     this.#label_top10.callback = () => {
       this.#cursor.timer = 0;
       fetch("http://localhost:5000/api/puzzles?count=10")
@@ -31,7 +29,7 @@ export default class PlayState extends State {
         .catch(err => console.error(err));
     };
 
-    this.#label_random = new Label(90, 8, "random");
+    this.#label_random = new Label("random", 90, 8);
     this.#label_random.callback = () => {
       this.#cursor.timer = 0;
       fetch("http://localhost:5000/api/puzzles?random=1&count=1")
@@ -45,13 +43,13 @@ export default class PlayState extends State {
         .catch(err => console.error(err));
     };
 
-    this.#label_recent = new Label(90, 16, "recent");
+    this.#label_recent = new Label("recent", 90, 16);
     this.#label_recent.callback = () => {};
 
-    this.#label_search = new Label(90, 24, "search");
+    this.#label_search = new Label("search", 90, 24);
     this.#label_search.callback = () => {};
 
-    this.#cursor = new Cursor(82, 0, 0.3, 0, 208);
+    this.#cursor = new Cursor(82, 0, 0, 0, 0, 24, 8, 0.3);
   }
 
   onEnter() {}
@@ -60,32 +58,16 @@ export default class PlayState extends State {
   init() {}
 
   update(dt) {
-    this.#cursor.timer += dt;
+   this.#cursor.update(dt);
 
-    if (KeyHandler.isDown(38)) {
-      if (this.#cursor.timer >= this.#cursor.delay) {
-        this.#cursor.timer = 0;
-        this.#cursor.y = clamp(this.#cursor.y - 8, 0, 24);
-      }
+   if (this.#cursor.selected) {
+    if (this.#cursor.y === this.#label_top10.y) {
+      this.#label_top10.callback();
     }
-    else if (KeyHandler.isDown(40)) {
-      if (this.#cursor.timer >= this.#cursor.delay) {
-        this.#cursor.timer = 0;
-        this.#cursor.y = clamp(this.#cursor.y + 8, 0, 24);
-      }
+    else if (this.#cursor.y === this.#label_random.y) {
+      this.#label_random.callback();
     }
-    else if (KeyHandler.isDown(13)) {
-      if (this.#cursor.timer >= this.#cursor.delay) {
-        this.#cursor.timer = 0;
-
-        if (this.#cursor.y === this.#label_top10.y) {
-          this.#label_top10.callback();
-        }
-        else if (this.#cursor.y === this.#label_random.y) {
-          this.#label_random.callback();
-        }
-      }
-    }
+   }
   }
 
   render() {
