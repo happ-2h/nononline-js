dbf8e8cdabde486b16d477f8f781b2ef05316399
happ-2h <hikonetgames@gmail.com>
2025-06-17 15:55:51 -0700

    Client Cursor redesign

--- client/src/gfx/ui/Cursor.js
+++ client/src/gfx/ui/Cursor.js
@@ -1,5 +1,6 @@
 import KeyHandler from "../../input/KeyHandler";
 import Renderer   from "../Renderer";
+import settings   from "../../game/settings";
 
 import { clamp }     from "../../math/utils";
 import { TILE_SIZE } from "../../game/constants";
@@ -13,100 +14,106 @@ export default class Cursor {
   #yMin;
   #yMax;
 
-  #step;
-
-  #sx;
-  #sy;
-
-  #delay;
-  #timer;
-  #animate;
-  #initialX;
+  #inputTimer;
+  #inputDelay;
 
   #selected;
+  #unselected;
+
+  #blink;
+  #blinkTimer;
+  #blinkSpeed;
+  #blinkState;
 
   constructor(
     x=0, y=0,
     xMin=0, xMax=0,
     yMin=0, yMax=0,
-    step=8, delay=0.3,
-    animate=true, flip=false
+    blink=false, blinkSpeed=0
   ) {
-    this.#x  = x;
-    this.#y  = y;
-    this.#sx = flip ? 32 : 0;
-    this.#sy = 208;
+    console.log(xMin, xMax);
+    this.#x = x;
+    this.#y = y;
 
     this.#xMin = xMin;
     this.#xMax = xMax;
     this.#yMin = yMin;
     this.#yMax = yMax;
 
-    this.#delay   = delay;
-    this.#timer   = 0;
-    this.#step    = step;
-    this.#animate = animate;
+    this.#inputTimer = 0;
+    this.#inputDelay = 0.2;
 
-    this.#initialX = x;
     this.#selected = false;
+    this.#unselected = false;
+
+    this.#blink = blink;
+    this.#blinkTimer = 0;
+    this.#blinkSpeed = blinkSpeed;
+    this.#blinkState = 0;
   }
 
   update(dt) {
-    this.#timer += dt;
-    this.#selected = false;
+    this.#inputTimer += dt;
 
-    if (KeyHandler.isDown(37)) {
-      if (this.#timer >= this.#delay) {
-        this.#timer = 0;
-        this.#x = clamp(this.#x - this.#step, this.#xMin, this.#xMax);
+    if (this.#inputTimer >= this.#inputDelay) {
+      if (KeyHandler.isDown(37) || KeyHandler.isDown(72)) {
+        this.#inputTimer = 0;
+        this.#blinkTimer = 0;
+        this.#blinkState = 1;
+        this.#x = clamp(this.#x - 8, this.#xMin, this.#xMax);
       }
-    }
-    else if (KeyHandler.isDown(39)) {
-      if (this.#timer >= this.#delay) {
-        this.#timer = 0;
-        this.#x = clamp(this.#x + this.#step, this.#xMin, this.#xMax);
+      else if (KeyHandler.isDown(39) || KeyHandler.isDown(76)) {
+        this.#inputTimer = 0;
+        this.#blinkTimer = 0;
+        this.#blinkState = 1;
+        this.#x = clamp(this.#x + 8, this.#xMin, this.#xMax);
       }
-    }
-    else if (KeyHandler.isDown(38)) {
-      if (this.#timer >= this.#delay) {
-        this.#timer = 0;
-        this.#y = clamp(this.#y - this.#step, this.#yMin, this.#yMax);
+      else if (KeyHandler.isDown(38) || KeyHandler.isDown(75)) {
+        this.#inputTimer = 0;
+        this.#blinkTimer = 0;
+        this.#blinkState = 1;
+        this.#y = clamp(this.#y - 8, this.#yMin, this.#yMax);
       }
-    }
-    else if (KeyHandler.isDown(40)) {
-      if (this.#timer >= this.#delay) {
-        this.#timer = 0;
-        this.#y = clamp(this.#y + this.#step, this.#yMin, this.#yMax);
-      }
-    }
-    else if (KeyHandler.isDown(13)) {
-      if (this.#timer >= this.#delay) {
-        this.#timer = 0;
-        this.#selected = true;
+      else if (KeyHandler.isDown(40) || KeyHandler.isDown(74)) {
+        this.#inputTimer = 0;
+        this.#blinkTimer = 0;
+        this.#blinkState = 1;
+        this.#y = clamp(this.#y + 8, this.#yMin, this.#yMax);
       }
     }
 
-    // Animation
-    if (this.#animate)
-      this.#x = this.#initialX + (4 * Math.cos(this.#timer * 20));
+    if (KeyHandler.isDown(32)) {
+      this.#selected = true;
+    }
+    else this.#selected = false;
+
+    if (KeyHandler.isDown(186)) {
+      this.#unselected = true;
+    }
+    else this.#unselected = false;
+
+    if (this.#blink) {
+      this.#blinkTimer += dt;
+
+      if (this.#blinkTimer >= this.#blinkSpeed) {
+        this.#blinkTimer = 0;
+        this.#blinkState = !this.#blinkState;
+      }
+    }
   }
 
   draw() {
-    Renderer.image(
-      "spritesheet",
-      this.#sx, this.#sy, TILE_SIZE, TILE_SIZE,
-      this.#x, this.#y, TILE_SIZE, TILE_SIZE
-    )
+    if (this.#blinkState) {
+      Renderer.image(
+        `${settings.theme}_theme`,
+        16, 8, TILE_SIZE, TILE_SIZE,
+        this.#x, this.#y, TILE_SIZE, TILE_SIZE
+      );
+    }
   }
 
-  get x()        { return this.#x; }
-  get y()        { return this.#y; }
-  get delay()    { return this.#delay; }
-  get timer()    { return this.#timer; }
+  get x() { return this.#x; }
+  get y() { return this.#y; }
   get selected() { return this.#selected; }
-
-  set x(_x)    { this.#x = _x; }
-  set y(_y)    { this.#y = _y; }
-  set delay(d) { this.#delay = d; }
-  set timer(t) { this.#timer = t; }
+  get unselected() { return this.#unselected; }
 };
