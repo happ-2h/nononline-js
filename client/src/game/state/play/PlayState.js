4d0b79025f8fbdd8cc0fe125455b9bbd8f67902d
happ-2h <hikonetgames@gmail.com>
2025-06-17 12:03:17 -0700

    Client SelectPuzzle state redesign

--- client/src/game/state/play/PlayState.js
+++ client/src/game/state/play/PlayState.js
@@ -1,19 +1,20 @@
+import Icon              from "../../../gfx/ui/Icon";
+import KeyHandler        from "../../../input/KeyHandler";
 import Label             from "../../../gfx/ui/Label";
+import Renderer          from "../../../gfx/Renderer";
+import Shortcut          from "../../../gfx/ui/Shortcut";
+import SelectPuzzleState from "./SelectPuzzleState";
+import settings          from "../../settings";
 import State             from "../State";
 import StateHandler      from "../StateHandler";
-import Shortcut from "../../../gfx/ui/Shortcut";
-import Icon from "../../../gfx/ui/Icon";
-import KeyHandler from "../../../input/KeyHandler";
+
 import { SCREEN_HEIGHT, TILE_SIZE } from "../../constants";
-import Renderer from "../../../gfx/Renderer";
-import settings from "../../settings";
 
 export default class PlayState extends State {
-  #shortcuts;
-
   #inputTimer;
   #inputDelay;
 
+  #shortcuts;
 
   constructor() {
     super();
@@ -27,14 +28,31 @@ export default class PlayState extends State {
         new Icon(0, 0, 48, 0),
         new Label("TOP 10", 0, 0),
         't',
-        () => {}
+        () => {
+          fetch("http://localhost:5000/api/puzzles?count=10")
+            .then(res => res.json())
+            .then(data => {
+              if (data.status === 200)
+                StateHandler.push(new SelectPuzzleState(data.data))
+            })
+            .catch(err => console.error(err));
+        }
       ),
       new Shortcut(
         8, 24, 14,
         new Icon(0, 0, 56, 0),
         new Label("RANDOM", 0, 0),
         'r',
-        () => {}
+        () => {
+          fetch("http://localhost:5000/api/puzzles?random=1&count=1")
+            .then(res => res.json())
+            .then(data => {
+              if (data.status === 200) {
+                StateHandler.push(new SelectPuzzleState([data.data]));
+              }
+            })
+            .catch(err => console.error(err));
+        }
       ),
       new Shortcut(
         8, 40, 14,
@@ -44,42 +62,6 @@ export default class PlayState extends State {
         () => StateHandler.pop()
       )
     ];
-
-    /*this.#label_top10 = new Label("TOP 10", 90, 0);
-    this.#label_top10.callback = () => {
-      this.#cursor.timer = 0;
-      fetch("http://localhost:5000/api/puzzles?count=10")
-        .then(res => res.json())
-        .then(data => {
-          if (data.status === 200) {
-            this.#cursor.timer = 0;
-            StateHandler.push(new SelectPuzzleState(data.data));
-          }
-        })
-        .catch(err => console.error(err));
-    };
-
-    this.#label_random = new Label("random", 90, 8);
-    this.#label_random.callback = () => {
-      this.#cursor.timer = 0;
-      fetch("http://localhost:5000/api/puzzles?random=1&count=1")
-        .then(res => res.json())
-        .then(data => {
-          if (data.status === 200) {
-            this.#cursor.timer = 0;
-            StateHandler.push(new SelectPuzzleState([data.data]));
-          }
-        })
-        .catch(err => console.error(err));
-    };
-
-    this.#label_recent = new Label("recent", 90, 16);
-    this.#label_recent.callback = () => {};
-
-    this.#label_search = new Label("search", 90, 24);
-    this.#label_search.callback = () => {};
-
-    this.#cursor = new Cursor(82, 0, 0, 0, 0, 24, 8, 0.3);*/
   }
 
   onEnter() {}
@@ -91,7 +73,18 @@ export default class PlayState extends State {
     this.#inputTimer += dt;
 
     if (this.#inputTimer >= this.#inputDelay) {
-      if (KeyHandler.isDown(81)) {
+      // Top 10
+      if (KeyHandler.isDown(84)) {
+        this.#inputTimer = 0;
+        this.#shortcuts[0].callback();
+      }
+      // Random
+      else if (KeyHandler.isDown(82)) {
+        this.#inputTimer = 0;
+        this.#shortcuts[1].callback();
+      }
+      // Quit
+      else if (KeyHandler.isDown(81)) {
         this.#inputTimer = 0;
         this.#shortcuts[2].callback();
       }
--- client/src/game/state/play/SelectPuzzleState.js
+++ client/src/game/state/play/SelectPuzzleState.js
@@ -1,23 +1,53 @@
-import Cursor       from "../../../gfx/ui/Cursor";
+import Icon         from "../../../gfx/ui/Icon";
+import KeyHandler   from "../../../input/KeyHandler";
 import Label        from "../../../gfx/ui/Label";
+import Shortcut     from "../../../gfx/ui/Shortcut";
 import SolveState   from "./SolveState";
 import State        from "../State";
 import StateHandler from "../StateHandler";
 
+import { SCREEN_WIDTH } from "../../constants";
+
 export default class SelectPuzzleState extends State {
+  #inputTimer;
+  #inputDelay;
+
   #puzzleData;
-  #labels;
-  #cursor;
+  #shortcuts;
 
   constructor(data) {
     super();
-    this.#puzzleData = [...data];
-    this.#labels = new Array(this.#puzzleData.length);
-    this.#cursor = new Cursor(42, 0, 0, 0, 0, (this.#puzzleData.length-1) * 8, 8, 0.3);
 
-    this.#puzzleData.forEach((puzzle, i) => {
-      this.#labels[i] = new Label(puzzle.title, 50, i * 8, puzzle.id);
+    this.#inputTimer = 0;
+    this.#inputDelay = 0.3;
+
+    this.#puzzleData = [...data];
+
+    this.#shortcuts = [];
+
+    data.forEach((puzzle, i) => {
+      this.#shortcuts.push(
+        new Shortcut(
+          SCREEN_WIDTH / 4 + 16,
+          8 + i * 16, 16,
+          new Icon(0, 0, 8, 0),
+          new Label(puzzle.title, 0, 0, puzzle.id),
+          `${i}`,
+          null
+        )
+      );
     });
+
+    this.#shortcuts.push(
+      new Shortcut(
+        SCREEN_WIDTH / 4 + 16,
+        8 + 10 * 16, 16,
+        new Icon(0, 0, 40, 0),
+        new Label("RETURN", 0, 0),
+        'q',
+        () => StateHandler.pop()
+      )
+    );
   }
 
   onEnter() {}
@@ -26,18 +56,80 @@ export default class SelectPuzzleState extends State {
   init() {}
 
   update(dt) {
-    this.#cursor.update(dt);
+    this.#inputTimer += dt;
 
-    if (this.#cursor.selected) {
-      const selectedPuzzle = this.#puzzleData[this.#cursor.y / 8];
-      StateHandler.pop();
-      StateHandler.push(new SolveState(selectedPuzzle));
+    if (this.#inputTimer >= this.#inputDelay) {
+      // 0 - 9
+      if (KeyHandler.isDown(48)) {
+        if (this.#puzzleData[0]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[0]));
+        }
+      }
+      else if (KeyHandler.isDown(49)) {
+        if (this.#puzzleData[1]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[1]));
+        }
+      }
+      else if (KeyHandler.isDown(50)) {
+        if (this.#puzzleData[2]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[2]));
+        }
+      }
+      else if (KeyHandler.isDown(51)) {
+        if (this.#puzzleData[3]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[3]));
+        }
+      }
+      else if (KeyHandler.isDown(52)) {
+        if (this.#puzzleData[4]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[4]));
+        }
+      }
+      else if (KeyHandler.isDown(53)) {
+        if (this.#puzzleData[5]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[5]));
+        }
+      }
+      else if (KeyHandler.isDown(54)) {
+        if (this.#puzzleData[6]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[6]));
+        }
+      }
+      else if (KeyHandler.isDown(55)) {
+        if (this.#puzzleData[7]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[7]));
+        }
+      }
+      else if (KeyHandler.isDown(56)) {
+        if (this.#puzzleData[8]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[8]));
+        }
+      }
+      else if (KeyHandler.isDown(57)) {
+        if (this.#puzzleData[9]) {
+          StateHandler.pop();
+          StateHandler.push(new SolveState(this.#puzzleData[9]));
+        }
+      }
+      // Quit
+      else if (KeyHandler.isDown(81)) {
+        this.#inputTimer = 0;
+        this.#shortcuts[this.#shortcuts.length - 1].callback();
+      }
     }
+
   }
 
   render() {
-    this.#labels.forEach(label => label.draw());
-
-    this.#cursor.draw();
+    this.#shortcuts.forEach(shortcut => shortcut.draw());
   }
 };
